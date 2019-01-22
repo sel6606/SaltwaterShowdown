151bf01c62b435217614bf0ed56ea86f7fc4fc71
nac5961 <nac5961@g.rit.edu>
2019-01-22 14:54:59 -0500

    Setup the architecture for the AI.

---
+++ Saltwater Showdown/Assets/Scripts/AIScripts/Action.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for all AI Actions
+/// </summary>
+public abstract class Action : ScriptableObject {
+
+    /// <summary>
+    /// Abstract method AI will use to perform various actions
+    /// </summary>
+    /// <param name="stateManager">Script attached to AI that manages switching between states</param>
+    public abstract void PerformAction(StateManager stateManager);
+}
---
+++ Saltwater Showdown/Assets/Scripts/AIScripts/Transition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains variables needed to switch states for the AI
+/// </summary>
+[System.Serializable]
+public class Transition {
+
+    public Decision decision;
+    public State nextState;
+}
