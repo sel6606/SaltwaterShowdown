786c1999f880275dc5ceef883305df3f9f1a94c4
Aiden <Aiden@DESKTOP-13MEF3D>
2019-02-05 23:05:43 -0500

    finished parallaxing

---
+++ Saltwater Showdown/Assets/Scripts/JellyFishUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyFishUp : MonoBehaviour {
+
+    public float speed;
+    public float bound;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - speed, transform.position.y + speed, transform.position.z);
+
+        if (transform.position.y > bound)
+        {
+            transform.position = new Vector3(-1f, -3, transform.position.z);
+        }
+    }
+}
